1b429bffe3e0f77d053f33ea4caf3b79f4d28aa5
zyurinbogdan <103205051+zyurinbogdan@users.noreply.github.com>
2022-11-16 09:45:29 +0300

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,6 +1,6 @@
 export default function shuffle (cards) {
     cards.forEach(card => {
-        let randomOrder = Math.floor(Math.random() * 12);
+        let randomOrder = Math.floor(Math.random() * 16);
         card.style.order = randomOrder;
     });
 }
